16a1f17a0f3afec8a8d2506469c1b9b155e7f7dc
Paul Anton Maximilian Wollenhaupt <77510444+unitdeterminant@users.noreply.github.com>
2022-01-30 15:39:13 +0100

    Add files via upload

--- viz.py
+++ viz.py
@@ -1,5 +1,20 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import os
+
+
+mpl.rcParams.update({
+    "text.color": "white",
+    "axes.labelcolor": "white",
+    "xtick.color": "white",
+    "ytick.color": "white",
+    "axes.edgecolor": "white"})
+
+
+def ensure_folder(path):
+    foldername = os.path.dirname(path)
+    if not os.path.exists(foldername):
+        os.makedirs(foldername)
 
 
 def simple_plots1d(trajectory, keys):
@@ -8,7 +23,11 @@ def simple_plots1d(trajectory, keys):
     for key in keys:
         plt.xlabel("time")
         plt.ylabel(key)
-
         plt.plot(time, trajectory[key][:, 0])
-        plt.savefig(os.path.join("plots", key + ".png"))
+        
+        path = os.path.join("plots", key + ".png")
+        ensure_folder(path)
+
+        plt.tight_layout()
+        plt.savefig(path, transparent=True)
         plt.close()
